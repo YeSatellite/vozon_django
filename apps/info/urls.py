2e30a64e531b8f49c24218148158f22e4d2bd1f7
Yernar Zharkynuly <yernar.zharkynuly@gmail.com>
2018-04-05 16:38:19 +0600

    transport settings

--- apps/info/urls.py
+++ apps/info/urls.py
@@ -6,10 +6,10 @@ from apps.info.views import CityViewSet, CountryViewSet, TransportTypeViewSet, T
 router = DefaultRouter()
 router.register(r'city', CityViewSet, base_name='city')
 router.register(r'country', CountryViewSet, base_name='location')
-router.register(r'transport/type', TransportTypeViewSet, base_name='transport_type')
-router.register(r'transport/mark', TransportMarkViewSet, base_name='transport_mark')
-router.register(r'transport/model', TransportModelViewSet, base_name='transport_model')
-router.register(r'transport/body', TransportBodyViewSet, base_name='transport_body')
-router.register(r'transport/shipping_type', TransportShippingTypeViewSet, base_name='transport_shipping_type')
+router.register(r'transport/type', TransportTypeViewSet, base_name='transport type')
+router.register(r'transport/mark', TransportMarkViewSet, base_name='transport mark')
+router.register(r'transport/model', TransportModelViewSet, base_name='transport model')
+router.register(r'transport/body', TransportBodyViewSet, base_name='transport body')
+router.register(r'transport/shipping-type', TransportShippingTypeViewSet, base_name='transport shipping type')
 
 urlpatterns = router.urls
