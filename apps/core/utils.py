02649d28fc7859407f6bf5cabc789557ac70fe2d
Yernar Zharkynuly <yernar.zharkynuly@gmail.com>
2018-04-07 18:58:28 +0600

    clear

--- apps/core/utils.py
+++ apps/core/utils.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 from django.conf import settings
+from rest_framework import status
 from rest_framework.views import exception_handler
 
 
@@ -7,6 +8,7 @@ def custom_exception_handler(exc, context):
     response = exception_handler(exc, context)
     error_code = getattr(settings, 'ERROR_CODE', None)
     if response and error_code:
+        response.status_code = status.HTTP_400_BAD_REQUEST
         assert isinstance(error_code, dict)
         for key, value in error_code.items():
             if isinstance(key, str):
