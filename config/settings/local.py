73a916b8dbd894eccbed88784cea516e04354ec7
Yernar Zharkynuly <yernar.zharkynuly@gmail.com>
2018-04-26 13:20:10 +0600

    asdas

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 from .base import *
 
-ALLOWED_HOSTS += ['192.168.1.236', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS += ['172.20.10.2', '127.0.0.1', 'localhost']
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'p_gnmp4v*+qks+rj)1d=(7v9-exb_e5vz41#l@-*etw4rtbzf('
