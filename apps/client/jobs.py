cb075584c639f046870ee26ecc31c7e20bed5a16
Yernar Zharkynuly <yernar.zharkynuly@gmail.com>
2018-05-09 17:52:11 +0600

    sdasdas

--- apps/client/jobs.py
+++ apps/client/jobs.py
@@ -2,16 +2,17 @@
 from datetime import datetime, timedelta
 from django_cron import CronJobBase, Schedule
 
-from apps.client.models import Order
+from apps.client.models import Order, Offer
 from apps.core.utils import norm
 
 
 class ClearOrderJob(CronJobBase):
-    schedule = Schedule(run_every_mins=4*60)
+    schedule = Schedule(run_every_mins=4 * 60)
     code = 'client.ClearOrderJob'
 
     def do(self):
         how_many_days = 1
         orders = Order.objects.filter(shipping_date__lte=datetime.now() - timedelta(days=how_many_days))
+        Offer.objects.filter(order__in=orders.values_list('id', flat=True)).delete()
         norm("deleted: %d" % len(orders))
         orders.delete()
