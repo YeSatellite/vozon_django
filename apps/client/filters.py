940206f4d3e8d7322e8f3e5ac3051d61373a39c3
glider <yesatellite@gmail.com>
2018-06-04 02:18:50 +0600

    add transport type

--- apps/client/filters.py
+++ apps/client/filters.py
@@ -17,13 +17,17 @@ class RouteFilterBackend(filters.BaseFilterBackend):
             queryset = queryset.filter(transport__isnull=True)
         elif type_:
             queryset = queryset.filter(transport__model__type=type_)
-        if par.get('start_point', None):
+        if f(par.get('start_point', None)):
             queryset = queryset.filter(start_point_id=par['start_point'])
-        if par.get('end_point', None):
+        if f(par.get('end_point', None)):
             queryset = queryset.filter(end_point_id=par['end_point'])
-        if par.get('start_date', None):
+        if f(par.get('start_date', None)):
             queryset = queryset.filter(shipping_date__gte=par['start_date'])
-        if par.get('end_date', None):
+        if f(par.get('end_date', None)):
             queryset = queryset.filter(shipping_date__lte=par['end_date'])
 
         return queryset
+
+
+def f(value):
+    return value and value != '0'
