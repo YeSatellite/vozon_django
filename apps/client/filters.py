c7920327d972708612bbf31b573799c3eb669bd7
glider <yesatellite@gmail.com>
2018-06-04 01:40:05 +0600

    route filter

--- apps/client/filters.py
+++ apps/client/filters.py
@@ -12,12 +12,18 @@ class RouteFilterBackend(filters.BaseFilterBackend):
         :type queryset: Route.objects
         """
         par = request.query_params
-        type_ = par.get('type', -1)
-        if type_ != -1:
-            queryset = queryset.filter(transport__type=par['type'])
-        else:
+        type_ = par.get('type', None)
+        if type_ == -1:
             queryset = queryset.filter(transport__isnull=True)
-        queryset = queryset.filter(start_point_id=par['start_point'])
-        queryset = queryset.filter(end_point_id=par['end_point'])
-        queryset = queryset.filter(shipping_date__range=[par['start_date'], par['end_date']])
+        elif type_:
+            queryset = queryset.filter(transport__model__type=type_)
+        if par.get('start_point', None):
+            queryset = queryset.filter(start_point_id=par['start_point'])
+        if par.get('end_point', None):
+            queryset = queryset.filter(end_point_id=par['end_point'])
+        if par.get('start_date', None):
+            queryset = queryset.filter(shipping_date__gte=[par['start_date']])
+        if par.get('end_date', None):
+            queryset = queryset.filter(shipping_date__lte=[par['end_date']])
+
         return queryset
