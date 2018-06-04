ae8ca43947f1ad9b6ebd1169bc8dcaf0384d5c18
glider <yesatellite@gmail.com>
2018-06-04 17:13:54 +0600

    log

--- apps/client/serializers.py
+++ apps/client/serializers.py
@@ -9,7 +9,7 @@ from apps.user.serializers import UserSerializer
 
 TRANSPORT_FIELDS = ('id', 'owner',
                     'model', 'shipping_type',
-                    'type_name', 'type_icon', 'mark_name', 'model_name', 'shipping_type_name',
+                    'type_name', 'mark_name', 'model_name', 'shipping_type_name',
                     'image1', 'image2', 'number', 'width', 'height', 'length', 'comment')
 
 
@@ -17,7 +17,7 @@ class TransportSerializer(serializers.ModelSerializer):
     owner = UserSerializer(read_only=True)
 
     type_name = serializers.ReadOnlyField(source='model.type.name')
-    type_icon = serializers.ReadOnlyField(source='model.type.icon')
+    # type_icon = serializers.ReadOnlyField(source='model.type.icon')
     mark_name = serializers.ReadOnlyField(source='model.mark.name')
     model_name = serializers.ReadOnlyField(source='model.name')
     shipping_type_name = serializers.ReadOnlyField(source='shipping_type.name')
