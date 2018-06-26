850c38b2244d3e7838a5f73d4f92d646665dd848
glider <yesatellite@gmail.com>
2018-06-26 18:07:51 +0600

    new admin tables

--- apps/client/admin.py
+++ apps/client/admin.py
@@ -1,3 +1,9 @@
+# coding=utf-8
 from django.contrib import admin
 
 # Register your models here.
+from apps.client.models import Order, Offer, Transport
+
+admin.site.register(Order)
+admin.site.register(Offer)
+admin.site.register(Transport)
